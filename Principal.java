33af50b00cb94dddb0a52ef484ee650d392fb2fe
Adriano Oliveira <104272390+atomoliveira@users.noreply.github.com>
2023-10-01 12:37:44 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,9 +1,14 @@
-
 public class Principal {
+    public static void main(String[] args) {
+        // Criar os itens do combo
+        Sanduiche sanduiche = new Sanduiche("Hamburguer Clássico");
+        Sobremesa sobremesa = new Sobremesa("Sundae de Chocolate");
+        Bebida bebida = new Bebida("Coca-Cola");
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        // Criar o combo
+        Combo combo = new Combo(sanduiche, sobremesa, bebida);
 
-	}
-
+        // Mostrar os itens do combo
+        combo.mostrarItensDoCombo();
+    }
 }
